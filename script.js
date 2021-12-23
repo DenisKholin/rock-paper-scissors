de3d79066f34e487683239268fa82c34e99d0bbc
Denis Kholin <den07.06@yandex.ru>
2021-12-23 22:36:42 +0300

    bad score

---
+++ script.js
@@ -0,0 +1,95 @@
+const
+	rockBtn = document.querySelector('#rockBtn'),
+	paperBtn = document.querySelector('#paperBtn'),
+	scissorsBtn = document.querySelector('#scissorsBtn'),
+	buttons = document.querySelectorAll('.button'),
+	meImg = document.querySelector('.me__img'),
+	compImg = document.querySelector('.comp__img'),
+	meScoreSelector = document.querySelector('.me__score'),
+	compScoreSelector = document.querySelector('.comp__score');
+
+let
+	meNum,
+	compNum,
+	meScore = 0,
+	compScore = 0,
+	result,
+	randomNumbers = {
+		0: 'img/rock.png',
+		1: 'img/paper.png',
+		2: 'img/scissors.png'
+	};
+
+
+
+function animation(selector) {
+	selector.classList.add('fade');
+}
+
+function setImg(selector, src) {
+	setTimeout(() => {
+		selector.setAttribute('src', src);
+		selector.classList.remove('fade');
+		console.log(result);
+		if (result == 'Вы проиграли') {
+			compScore += 1;
+			compScoreSelector.innerHTML = `${compScore}`;
+		} else if (result == 'Вы выиграли') {
+			meScore += 1;
+			meScoreSelector.innerHTML = `${meScore}`;
+		}
+	}, 2000);
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function prepare() {
+	compNum = getRandomInt(0, 3);
+	animation(meImg);
+	animation(compImg);
+	setImg(compImg, randomNumbers[compNum]);
+}
+
+
+rockBtn.addEventListener('click', () => {
+	prepare();
+	setImg(meImg, 'img/rock.png');
+	if (compNum == 0) {
+		result = 'Ничья';
+	} else if (compNum == 1) {
+		result = 'Вы проиграли';
+
+	} else {
+		result = 'Вы выиграли';
+
+	}
+});
+
+paperBtn.addEventListener('click', () => {
+	prepare();
+	setImg(meImg, 'img/paper.png');
+	if (compNum == 0) {
+		result = 'Вы выиграли';
+	} else if (compNum == 1) {
+		result = 'Ничья';
+	} else {
+		result = 'Вы проиграли';
+	}
+});
+
+
+scissorsBtn.addEventListener('click', () => {
+	prepare();
+	setImg(meImg, 'img/scissors.png');
+	if (compNum == 0) {
+		result = 'Вы проиграли';
+	} else if (compNum == 1) {
+		result = 'Вы выиграли';
+	} else {
+		result = 'Ничья';
+	}
+});
