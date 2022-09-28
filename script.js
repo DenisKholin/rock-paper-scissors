85fae0c8d18f64fe2eba464449df899af2f15ee4
Denis Kholin <den07.06@yandex.ru>
2022-09-29 00:13:28 +0300

    JS refactoring

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 const
-	rockBtn = document.querySelector('#rockBtn'),
-	paperBtn = document.querySelector('#paperBtn'),
-	scissorsBtn = document.querySelector('#scissorsBtn'),
-	buttons = document.querySelectorAll('.button'),
 	meImg = document.querySelector('.me__img'),
 	compImg = document.querySelector('.comp__img'),
 	meScoreSelector = document.querySelector('.me__score'),
@@ -10,10 +6,10 @@ const
 
 let
 	compNum,
+	result,
 	meScore = 0,
 	compScore = 0,
-	result,
-	randomNumbers = {
+	imgNumb = {
 		0: 'img/rock.png',
 		1: 'img/paper.png',
 		2: 'img/scissors.png'
@@ -23,14 +19,18 @@ function animation(selector) {
 	selector.classList.add('fade');
 }
 
+function switchImg(selector, src) {
+	selector.setAttribute('src', src);
+	selector.classList.remove('fade');
+}
+
 function setMyImg(src) {
 	setTimeout(() => {
-		meImg.setAttribute('src', src);
-		meImg.classList.remove('fade');
-		if (result == 'Вы проиграли') {
+		switchImg(meImg, src)
+		if (result == -1) {
 			compScore += 1;
 			compScoreSelector.innerHTML = `${compScore}`;
-		} else if (result == 'Вы выиграли') {
+		} else if (result == 1) {
 			meScore += 1;
 			meScoreSelector.innerHTML = `${meScore}`;
 		}
@@ -39,59 +39,31 @@ function setMyImg(src) {
 
 function setCompImg(src) {
 	setTimeout(() => {
-		compImg.setAttribute('src', src);
-		compImg.classList.remove('fade');
+		switchImg(compImg, src)
 	}, 2000);
 }
 
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min;
-}
-
 function prepare() {
-	compNum = getRandomInt(0, 3);
+	compNum = (Math.floor(Math.random() * 3));
 	animation(meImg);
 	animation(compImg);
-	setCompImg(randomNumbers[compNum]);
-	console.log(randomNumbers[compNum]);
+	setCompImg(imgNumb[compNum]);
 }
 
-rockBtn.addEventListener('click', () => {
-	prepare();
-	setMyImg('img/rock.png');
-	if (compNum == 0) {
-		result = 'Ничья';
-	} else if (compNum == 1) {
-		result = 'Вы проиграли';
-	} else {
-		result = 'Вы выиграли';
+function play(btnSelector, img, win, lose) {
+	document.querySelector(btnSelector).addEventListener('click', () => {
+		prepare();
+		setMyImg(img);
+		if (compNum == win) {
+			result = 1;
+		} else if (compNum == lose) {
+			result = -1
+		} else {
+			result = 0
+		}
+	})
+}
 
-	}
-});
-
-paperBtn.addEventListener('click', () => {
-	prepare();
-	setMyImg('img/paper.png');
-	if (compNum == 0) {
-		result = 'Вы выиграли';
-	} else if (compNum == 1) {
-		result = 'Ничья';
-	} else {
-		result = 'Вы проиграли';
-	}
-});
-
-scissorsBtn.addEventListener('click', () => {
-	prepare();
-	setMyImg('img/scissors.png');
-	if (compNum == 0) {
-		result = 'Вы проиграли';
-	} else if (compNum == 1) {
-		result = 'Вы выиграли';
-	} else {
-		result = 'Ничья';
-	}
-});
+play('#rockBtn', imgNumb[0], 2, 1);
+play('#paperBtn', imgNumb[1], 0, 2);
+play('#scissorsBtn', imgNumb[2], 1, 0);
